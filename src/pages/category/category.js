181ca0fc8b75a81ecbf87d04d5569c58a59e69ff
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-04 09:18:32 -0300

    DOCS: files in main page category modified

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import HeaderDesktop from '../../components/header-Desktop/header-Desktop';
 import CategorySelector from '../../components/category-Selector/category-Selector';
 import ProductCard from '../../components/product-Card/product-Card';
-import './index.css'
+import './category.css'
 
 export class Category extends Component {
     
     render() {
         return (
             <div className="mainContainer">
-                <HeaderDesktop />
                 <CategorySelector />
                 <div className='productsContainer'>
                     <ProductCard />
