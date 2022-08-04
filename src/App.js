e2e0879fa9df5632c65f438cc4b947ede9d1599e
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-04 09:22:37 -0300

    DOCS: main files modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HeaderDesktop from './components/header-Desktop/header-Desktop';
+import Category from "./pages/category/category";
+import Cart from './pages/cart/cart'
+import ProductDetail from './components/product-Detail/product-Detail';
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <HeaderDesktop />
+        <Routes>
+          <Route path='/' element={ < Category /> } />
+          <Route exact path='/cart' element={ < Cart /> } />
+          <Route exact path='/productdetails/:id' element={ <ProductDetail />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
